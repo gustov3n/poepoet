363ccc49d6cffe9ae96a4a6d250577c9d35a7cd3
Agustova <gustov3n@gmail.com>
2022-07-26 20:42:57 +0700

    initial commit

---
+++ app/src/main/java/com/example/kantinku/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.kantinku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToOnboarding(View v) {
+        Intent intent = new Intent(this, Onboarding.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/kantinku/Onboarding.java
@@ -0,0 +1,20 @@
+package com.example.kantinku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Onboarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+
+    void goToNextPage(View v) {
+
+    }
+}
